2123f31538cd029571067d8c68f293c6308816d3
Vikram Negi <viknegi0@gmail.com>
2021-09-24 14:07:13 +0530

    express app

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 "use strict";
+require("dotenv").config();
 const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Aliens!");
+});
+
+app.listen(port, () => console.log(`listening at http://localhost:${port}`));
