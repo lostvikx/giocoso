b56acb2ab50799a9c5c8ae4a74812f1eb0fb02c1
Vikram Negi <viknegi0@gmail.com>
2021-09-25 18:27:24 +0530

    sexy scrollToTop btn & showcase

--- public/js/scrollup.js
+++ public/js/scrollup.js
@@ -3,14 +3,29 @@
 const scrollBtn = document.getElementById("scroll-up-btn");
 const rootElement = document.documentElement;
 
-const scrollToTop = () => {
+function scrollToTop() {
   rootElement.scrollTo({
     top: 0,
     behavior: "smooth"
   });
-};
+}
 
 scrollBtn.addEventListener("click", (e) => {
   e.preventDefault();
   scrollToTop();
 });
+
+const heightPixels = 500;
+
+function showScrollBtn() {
+  // For debugging
+  // console.log(rootElement.scrollTop);
+
+  if (rootElement.scrollTop > heightPixels || document.body.scrollTop > heightPixels) {
+    scrollBtn.style.visibility = "visible";
+  } else {
+    scrollBtn.style.visibility = "hidden";
+  }
+}
+
+document.addEventListener("scroll", showScrollBtn);
