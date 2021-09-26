af9790eb2d12166cf06fbfdf52605fe9164bfb83
Vikram Negi <viknegi0@gmail.com>
2021-09-26 16:37:31 +0530

    added some comments

--- public/js/scrollup.js
+++ public/js/scrollup.js
@@ -3,6 +3,7 @@
 const scrollBtn = document.getElementById("scroll-up-btn");
 const rootElement = document.documentElement;
 
+// The scroll funciton
 function scrollToTop() {
   rootElement.scrollTo({
     top: 0,
@@ -17,6 +18,7 @@ scrollBtn.addEventListener("click", (e) => {
 
 const heightPixels = 500;
 
+// Show btn on heightPixels (this needs to be modified accordingly)
 function showScrollBtn() {
   // For debugging
   // console.log(rootElement.scrollTop);
