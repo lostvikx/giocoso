2d10842755c959bd162ede3194142ea47beb08b8
Vikram Negi <viknegi0@gmail.com>
2021-09-27 13:46:46 +0530

    showcase text animation

---
+++ public/js/animate.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const word = document.getElementById("adjective");
+const adjectives = ["Managed", "Made", "Packaged", "Produced", "Created", "Leveled", "Hyped"];
+
+function selectRand(arr) {;
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function changeAdj(newWord) {
+  word.textContent = newWord;
+
+  if (word.className == "bg-in") {
+    word.className = "bg-out";
+  } else {
+    word.className = "bg-in";
+  }
+
+  // For debug
+  console.log(word.className);
+}
+
+setInterval(() => changeAdj(selectRand(adjectives)), 5000);
